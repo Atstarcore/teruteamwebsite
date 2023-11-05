2e1f12bed1674699e738a607c6d843b8a4298c03
Atstarcore <141202880+Atstarcore@users.noreply.github.com>
2023-11-05 22:12:44 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# teruteam-beta
+stable: https://teruteam.vercel.app/
+
+# Thanks to:
+Thanh Tran\
+TunaVoNo\
+Atstarcore\
+Tin Thak\
+tamkdz (gay vcl) (thg nay lam dc j. asc said)
---
+++ src/script.js
@@ -0,0 +1,10 @@
+var a = ('du e di vao ngo nho')
+var hoatDong = a
+
+if (hoatDong = a) {
+    console.log('dit nhau tren cay cau anh so em bi dau em quay ra dang truoc anh dit ut dang sau')
+}else{
+    console.log ('keep yourself safe')
+}
+
+
